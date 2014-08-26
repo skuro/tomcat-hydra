203be5c3f69db0ea9212cea39b65950c3dc1de4b
Carlo Sciolla <skuro@skuro.tk>
2014-08-26 09:49:46 +0200

    Base set

---
+++ editorial/bin/editorial.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+export CATALINA_HOME="/Users/skuro/Development/Backbase/envs/apache-tomcat-7.0.55"
+export CATALIN_BASE="${CATALINA_HOME}/editorial"
+
+function usage(){
+    echo "$0 [start|stop]"
+}
+
+if [ $# -lt 1 ]
+then
+    usage
+    exit -1
+fi
+
+ACTION=$1
+
+function start(){
+    ${CATALINA_HOME}/bin/startup.sh
+}
+
+function stop(){
+    ${CATALINA_HOME}/bin/shutdown.sh
+}
+
+case "$ACTION" in
+    "start")
+        start
+        ;;
+    "stop")
+        stop
+        ;;
+esac
